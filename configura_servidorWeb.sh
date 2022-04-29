b6a2960bc288167c7c60d5bcda03ae2c8450f9cb
robsonluis1608 <90524131+robsonluis1608@users.noreply.github.com>
2022-04-29 16:55:53 -0300

    Segundo commit (script completo)

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Prova de Conceito 01 - Robson Luis Silva
+echo "Prova de Conceito 01 - Robson Luis Silva"
 
 #Etapa 01 - Atualizando pacotes
 apt-get update
@@ -14,3 +15,24 @@ apt-get install -y wget
 
 #Etapa 04 - Download do arquivo .zip
 wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+#Etapa 05 - Instalando pacote (zip e unzip)
+apt-get install -y zip unzip
+# Descompacta o arquivo zip
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+# Entra na pasta descompactada
+cd 873219-690e1f604dc8ead2583589e1aee6f8a42040a035 
+# Copia os arquivos para outro diretório
+cp * /var/www/html/
+
+#Etapa 06 - Entrando na pasta (/var/www/html/)
+cd /var/www/html/
+
+#Etapa 07 - Removendo arquivo index.html da pasta
+rm index.html
+
+#Etapa 08 - Renomeando arquivo (cv-resumido.html para index.html)
+mv cv-resumido.html index.html
+
+#Etapa 09 - Iniciando servidor web Apache
+/etc/init.d/apache2 start
